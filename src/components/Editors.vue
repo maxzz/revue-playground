e3224ccbf25c2afa2568a05cec62c9892453713d
Max Zakharzhevskiy <maxzz@msn.com>
2021-02-24 19:59:43 -0800

    Feature: Add textarea

--- src/components/Editors.vue
+++ src/components/Editors.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="mx-4 flex mb-4">
+        <textarea class="border flex-1" :rows="10"></textarea>
+    </div>
     <div class="flex px-4 mb-4">
         <input class="flex-1 px-3 py-2 border rounded text-gray-600 mr-2" value="abc">
         <input class="flex-1 px-3 py-2 border rounded text-gray-600" value="abc">
