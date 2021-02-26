b08aa66332201611300839a3c9d8bf94b9f8595a
Max Zakharzhevskiy <maxzz@msn.com>
2021-02-25 18:37:23 -0800

    Feature: add codemirror editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,28 @@
+<template>
+    <div ref="elEditor">
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, onMounted, ref } from 'vue';
+    import cm from 'codemirror';
+    import "codemirror/lib/codemirror.css";
+
+    export default defineComponent({
+        setup() {
+            const elEditor = ref();
+
+            onMounted(() => {
+                console.log({elEditor});
+                const myEditor = cm(elEditor.value, {
+                    value: 'function test()\n{\n}\n',
+                    mode: 'javascript'
+                });
+            });
+
+            return {
+                elEditor
+            };
+        }
+    });
+</script>
--- src/components/Editors.vue
+++ src/components/Editors.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="mx-4 flex mb-4">
-        <textarea class="border flex-1" :rows="10"></textarea>
+    <div class="split-panel flex px-4">
+        <Editor class="flex-1 border border-gray-200" />
+        <div class="flex-grow-0 flex-shring-0 w-1"></div>
+        <Editor class="flex-1 border border-green-200" />
+    </div>
+
+    <div class="mx-4 flex my-4">
+        <textarea class="border flex-1" :rows="3"></textarea>
     </div>
     <div class="flex px-4 mb-4">
         <input class="flex-1 px-3 py-2 border rounded text-gray-600 mr-2" value="abc">
@@ -16,9 +22,11 @@
     import { ref, defineComponent } from "vue";
     import { start } from './convert';
     import { useToast } from "vue-toastification";
+    import Editor from './Editor.vue';
 
     export default defineComponent({
         props: {},
+        components: { Editor },
         setup: () => {
             console.log('start');
 
