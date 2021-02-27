62fee7c36241be68a3040e097a8996857fd63fd6
Max Zakharzhevskiy <maxzz@msn.com>
2021-02-26 18:25:23 -0800

    Feature: add splitter upgrade in progress

--- src/components/SplitPane.vue
+++ src/components/SplitPane.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex px-4">
-        <div class="flex-1 bg-red-200">
+    <div ref="container" class="flex px-4">
+        <div class="bg-red-200" :style="styleA">
             <slot name="a">
                 default a
             </slot>
         </div>
-        <div class="devider w-1 flex-grow-0 flex-shrink-0"></div>
-        <div class="flex-1 bg-green-200">
+        <div class="devider w-1 flex-grow-0 flex-shrink-0 cursor-col-resize" @mousedown="onMouseDown"></div>
+        <div class="bg-green-200" :style="styleB">
             <slot name="b">
                 default b
             </slot>
@@ -15,11 +15,80 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue";
+    import { defineComponent, reactive, ref } from "vue";
 
     export default defineComponent({
         setup() {
-            return {};
+            const position = ref(0);
+            let onResize: (() => void) | null = null;
+            const container = ref<HTMLElement>();
+            const vertical = true;
+
+            const onMouseDown = function(event: MouseEvent) {
+                event.preventDefault(); // This is needed to prevent text selection in Safari
+
+                const elContainer = container.value as HTMLElement;
+                const offset = vertical ? elContainer.offsetTop : elContainer.offsetLeft;
+                const size = vertical ? elContainer.offsetHeight : elContainer.offsetWidth;
+                
+                document.body.style.cursor = vertical ? 'row-resize' : 'col-resize';
+
+                let moveHandler = (event: MouseEvent) => {
+                    event.preventDefault();
+                    let newPosition = (((vertical ? event.pageY : event.pageX) - offset) / size) * 100;
+                    newPosition = Math.floor(Math.min(Math.max(0, newPosition), 99)); // Using 99% as the max value prevents the divider from disappearing
+                    setPosition(newPosition);
+                };
+
+                let upHandler = () => {
+                    document.removeEventListener('mousemove', moveHandler);
+                    document.removeEventListener('mouseup', upHandler);
+                    document.body.style.cursor = '';
+
+                    if (onResize) {
+                        onResize();
+                    }
+                };
+
+                document.addEventListener('mousemove', moveHandler);
+                document.addEventListener('mouseup', upHandler);
+            };
+
+            function setPosition(x: number) {
+                console.log('setpos', x);
+                position.value = x;
+
+                if (vertical) {
+                    styleA.minHeight = styleA.maxHeight = position + '%'; // top
+                } else {
+                    styleA.minWidth = styleA.maxWidth = position + '%'; // left
+                }
+            }
+
+            type styleAB = {
+                flex: string;
+                minWidth?: string;
+                minHeight?: string;
+                maxWidth?: string;
+                maxHeight?: string;
+            }
+
+            const styleA = reactive<styleAB>({
+                flex: '1',
+            });
+
+            const styleB = reactive<styleAB>({
+                flex: '1',
+                minWidth: '0',
+                minHeight: '0',
+            });
+
+            return {
+                container,
+                styleA,
+                styleB,
+                onMouseDown,
+            };
         },
     });
 </script>
--- windi.config.ts
+++ windi.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   shortcuts: {
     btn: 'px-4 py-2 border border-solid border-gray-300 rounded text-gray-600 hover:text-blue-600 hover:shadow active:bg-gray-200 focus:outline-none focus:ring-1 focus:ring-blue-600',
   },
+  theme: {
+      cursor: {
+          'col-resize': 'col-resize',
+          'row-resize': 'row-resize',
+      }
+  },
 //   theme: {
 //     extend: {
 //       colors: {
