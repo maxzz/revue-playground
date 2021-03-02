7749f1270bf9689fa3a206395c19cda1fbec77ea
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-02 01:02:07 -0800

    Feature: add header layout update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex mx-4 justify-end  opacity-50 bg-gradient-to-r from-green-100 to-green-300">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 215 128">
+    <div class="mx-4 py-2 flex justify-between items-center opacity-80 bg-gradient-to-r from-green-100 to-green-300">
+        <span class="pl-1 text-black font-bold text-xl">ReVue</span>
+        <svg class="mr-1 w-12 h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 215 128">
             <path class="logo-decomp-a" d="M87 0h128v128H87z" />
             <circle class="logo-decomp-circle" cx="64" cy="64" r="64" />
             <path class="logo-decomp-arrow" d="M157 64L87 1v126l70-63z"/>
@@ -10,10 +11,6 @@
 </template>
 
 <style scoped>
-    svg {
-        width: 44px;
-        height: 24px;
-    }
     .logo-decomp-a {
         fill: #0fbe58;
     }
